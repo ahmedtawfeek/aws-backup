9ec36a531616cb67be73fc0f7ad80fab5c1f7cf0
ahmedtawfeek <44138882+ahmedtawfeek@users.noreply.github.com>
2024-02-06 19:18:51 +0200

    Update aws_backup.sh

--- aws_backup.sh
+++ aws_backup.sh
@@ -7,9 +7,10 @@ DB_HOST=$1
 DB_PASSWORD=$2
 AWS_SECRET=$3
 BUCKET_NAME=$4
+AWS_KEY=$5
 
 mysqldump -u root -h $DB_HOST -p$DB_PASSWORD --all-databases > /tmp/$BACKUP  && \
-export AWS_ACCESS_KEY_ID=AKIA3OT5MYDWPN53TKTP && \
+export AWS_ACCESS_KEY_ID=$AWS_KEY && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Uploading your $BACKUP backup" && \
 aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
